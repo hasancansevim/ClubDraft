2584b587575396e0e1b7209fdcba1a9824c2400a
Hasan Can Sevim <shasancan0@gmail.com>
2026-08-07 03:18:26 +0300

    Configure YARP API Gateway

--- src/ApiGateway/Program.cs
+++ src/ApiGateway/Program.cs
@@ -1,6 +1,24 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add YARP Reverse Proxy
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    // Developer exception page
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseRouting();
+
+// Enable YARP routing
+app.MapReverseProxy();
 
 app.Run();
